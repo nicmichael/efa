4fb879faf61b364ee112fac354cd2b30838905e7
tfyh <info@tfyh.org>
2020-10-04 10:48:37 +0200

    This is just a commit to give github a try. Committed using Intellij Idea

--- de/nmichael/efa/data/efacloud/CsvCodec.java
+++ de/nmichael/efa/data/efacloud/CsvCodec.java
@@ -1,3 +1,12 @@
+/**
+ * Title:        efa - elektronisches Fahrtenbuch für Ruderer
+ * Copyright:    Copyright (c) 2001-2011 by Nicolas Michael
+ * Website:      http://efa.nmichael.de/
+ * License:      GNU General Public License v2
+ *
+ * @author Nicolas Michael, Martin Glade
+ * @version 2
+ */
 package de.nmichael.efa.data.efacloud;
 
 import java.util.ArrayList;
