255f7d63ba63785119a50cff2cfd02e3380a984e
Nicolas Michael <email@nmichael.de>
2021-03-18 00:23:44 -0700

    Version 2.3.0_01

--- de/nmichael/efa/Daten.java
+++ de/nmichael/efa/Daten.java
@@ -42,8 +42,8 @@ import javax.swing.plaf.ColorUIResource;
 public class Daten {
 
     public final static String VERSION            = "2.3.0"; // Version für die Ausgabe (z.B. 2.1.0, kann aber auch Zusätze wie "alpha" o.ä. enthalten)
-    public final static String VERSIONID          = "2.3.0_00";   // VersionsID: Format: "X.Y.Z_MM"; final-Version z.B. 1.4.0_00; beta-Version z.B. 1.4.0_#1
-    public final static String VERSIONRELEASEDATE = "16.03.2021";  // Release Date: TT.MM.JJJJ
+    public final static String VERSIONID          = "2.3.0_01";   // VersionsID: Format: "X.Y.Z_MM"; final-Version z.B. 1.4.0_00; beta-Version z.B. 1.4.0_#1
+    public final static String VERSIONRELEASEDATE = "18.03.2021";  // Release Date: TT.MM.JJJJ
     public final static String MAJORVERSION       = "2";
     public final static String PROGRAMMID         = "EFA.230"; // Versions-ID für Wettbewerbsmeldungen
     public final static String PROGRAMMID_DRV     = "EFADRV.230"; // Versions-ID für Wettbewerbsmeldungen
