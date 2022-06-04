b29beeab654dbf38f64aec9b47fb4e3359e3c5fd
cubitusgh2 <73741388+cubitusgh2@users.noreply.github.com>
2022-06-04 23:08:42 +0200

    EFA_0019_TO_23_TABLES_UPGRADE_PACKAGE (#19)
    * EFA_0019_TO_23_TABLES_UPGRADE_PACKAGE
    
    Resizable DataList dialogs
    -----------------------------------
    ItemTypeTable.java
    - all tables in the *ListDialog classes now support growing when the dialog is resized to bigger dimensions.
    
    Better readability for all tables
    -----------------------------------
    Table.java
    - add 4 pixels to current font size and use this as the row height
    
    ShowLogbookDialog.java
    - add 4 pixels to current font size and use this as the row height
    - supports row height changes for nested tables
    - No further changes as this would require TableCellRenderer.java to be renamed to avoid conflicts to interface javax.*.TableCellRenderer;
    
    TableCellrenderer.java
    - each cell has some whitespace on the left and right side.
    
    BoatDamageList.java
    - set minimum widths for date fields so that at least date value is displayed
    
    Bold Table Headers
    -----------------------------------
    TableHeaderCellRendererBold
    - new class
    
    Table.java
    - set the bold table header renderer
    
    ShowLogbookDialog.java
    - activate new bold table header
    
    Auto tooltip when data does not fit into cell
    -----------------------------------
    Table.java
    - getToolTipText: return Tooltiptext only if data does not fit into cell
    - activation of auto tooltips dependent on config setting
    
    ItemTypeTable.java
    - activate
    
    EfaConfig.java
    - new public property COMMON/Tabellen mit Tooltipps, default true
    
    efa_de.properties
    - new item Tabellen_mit_Tooltipps_f\u00fcr_zu_lange_Texte
    
    efa_en.properties
    - new item Tabellen_mit_Tooltipps_f\u00fcr_zu_lange_Texte
    
    Alternating Colors
    -----------------------------------
    Table.java
    - alternating colors dependent on config setting
    - set inverted selection color
    
    TableCellRenderer.java
    - alternating colors dependent on config setting
    - alternating colors
    - disabled items which are selected get italic font style (disabled text color has bad contrast to inverted selection color)
    
    efa_de.properties
    - new item Tabellen_mit_alternierenden_Zeilenfarben
    
    efa_en.properties
    - new item Tabellen_mit_alternierenden_Zeilenfarben
    
    * EFA_0024_FILTERFIELD_BOATS_AND_PERSONS_REPLAY
    
    - Each list in efa boathouse has a filter textfield assigned
    - filters case-insensitive and also in mode "contains"
    - ESC in filter textfield clears the filter
    - STRG-F in the corresponding list focuses the filter text field
    - VK_DOWN, VK_ENTER in filter textfield sets focus in the data list
    - when a filter text is set AND the list gets updated (e.g. by cancelling an active tour, the list of the available boats gets updated), the list is updated with the filter
    - old "scrolltoentry" function still works.
    - configuration option: any boathouse list can be configured wether to have a filter field or not. By default, available boats, boats on session and person list do have a filter text field.
    
    * Update EfaConfig.java
    
    As requested by nicmichael, new options for tables (alternating row color, tooltipps) moved to "TYPE_EXPERT".
    
    * Less config items, config items are TYPE_EXPERT
    
    Reduced config values for filter textfields above lists from 4 to 2:
    
    - Common lists (available boats, boats on the water, persons)
    - not available boats
    The latter is because filtering in this does not make sense for smaller clubs and the text field wastes vertical screen size on smaller screens.
    
    Config items are TYPE_EXPERT
    
    Moved the new config items to an earlier position in the "Boathouse" category (they are more likely to be searched for by the admin at that position).
    
    Updated international support and removed no longer needed elements of this feature.

--- de/nmichael/efa/gui/EfaBoathouseFrame.java
+++ de/nmichael/efa/gui/EfaBoathouseFrame.java
@@ -477,10 +477,10 @@ public class EfaBoathouseFrame extends BaseFrame implements IItemListener {
         toggleAvailableBoatsToPersons.setVisible(Daten.efaConfig.getValueEfaDirekt_listAllowToggleBoatsPersons());
 
         // Boat Lists
-        boatsAvailableList = new ItemTypeBoatstatusList("BOATSAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("verfügbare Boote"), this);
-        personsAvailableList = new ItemTypeBoatstatusList("PERSONSAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("Personen"), this);
-        boatsOnTheWaterList = new ItemTypeBoatstatusList("BOATSONTHEWATERLIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("Boote auf Fahrt"), this);
-        boatsNotAvailableList = new ItemTypeBoatstatusList("BOATSNOTAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("nicht verfügbare Boote"), this);
+        boatsAvailableList = new ItemTypeBoatstatusList("BOATSAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("verfügbare Boote"), this, Daten.efaConfig.getValueEfaBoathouseFilterTextfieldStandardLists());
+        personsAvailableList = new ItemTypeBoatstatusList("PERSONSAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("Personen"), this,Daten.efaConfig.getValueEfaBoathouseFilterTextfieldStandardLists());
+        boatsOnTheWaterList = new ItemTypeBoatstatusList("BOATSONTHEWATERLIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("Boote auf Fahrt"), this,Daten.efaConfig.getValueEfaBoathouseFilterTextfieldStandardLists());
+        boatsNotAvailableList = new ItemTypeBoatstatusList("BOATSNOTAVAILABLELIST", IItemType.TYPE_PUBLIC, "", International.getStringWithMnemonic("nicht verfügbare Boote"), this,Daten.efaConfig.getValueEfaBoathouseFilterTextfieldBoatsNotAvailableList());
         boatsAvailableList.setFieldSize(200, 400);
         personsAvailableList.setFieldSize(200, 400);
         boatsOnTheWaterList.setFieldSize(200, 300);
