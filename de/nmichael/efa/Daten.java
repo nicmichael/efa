200f8120c76ae9181e0ad816df0ec1bb38c9c099
cubitusgh2 <73741388+cubitusgh2@users.noreply.github.com>
2026-04-27 20:49:05 +0200

    Update eou and version number

--- de/nmichael/efa/Daten.java
+++ de/nmichael/efa/Daten.java
@@ -75,15 +75,15 @@ import de.nmichael.efa.util.Logger;
 public class Daten {
 
 	private static final String KEY_VALUE_DELIMITER = " = ";
-	public final static String VERSION = "2.5.1"; // Version für die Ausgabe (z.B. 2.1.0, kann aber
+	public final static String VERSION = "2.5.3"; // Version für die Ausgabe (z.B. 2.1.0, kann aber
 																	// auch Zusätze wie "alpha" o.ä. enthalten)
 
-	public final static String VERSIONID = "2.5.1_#10"; // VersionsID: Format: "X.Y.Z_MM"; final-Version z.B. 1.4.0_00;
+	public final static String VERSIONID = "2.5.3_#1"; // VersionsID: Format: "X.Y.Z_MM"; final-Version z.B. 1.4.0_00;
 														// beta-Version z.B. 1.4.0_#1  //# is not good, is used in efa.data.Waters 
-	public final static String VERSIONRELEASEDATE = "19.04.2026"; // Release Date: TT.MM.JJJJ
+	public final static String VERSIONRELEASEDATE = "26.04.2026"; // Release Date: TT.MM.JJJJ
 	public final static String MAJORVERSION = "2";
-	public final static String PROGRAMMID = "EFA.251"; // Versions-ID für Wettbewerbsmeldungen
-	public final static String PROGRAMMID_DRV = "EFADRV.251"; // Versions-ID für Wettbewerbsmeldungen
+	public final static String PROGRAMMID = "EFA.253"; // Versions-ID für Wettbewerbsmeldungen
+	public final static String PROGRAMMID_DRV = "EFADRV.253"; // Versions-ID für Wettbewerbsmeldungen
 	public final static String COPYRIGHTYEAR = "26"; // aktuelles Jahr (Copyright (c) 2001-COPYRIGHTYEAR)
 	public final static int REQUIRED_JAVA_VERSION = 17;
 
